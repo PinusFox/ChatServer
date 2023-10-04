a9f3da4c78cf877283b3b3d42c649224eeb3f755
PinusFox <greiner.roli@gmail.com>
2023-10-04 16:49:22 +0200

    connect

--- Program.cs
+++ Program.cs
@@ -1,32 +1,72 @@
 ﻿using System.Threading;
 using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
-namespace Program
+class Server
 {
-    class ChatServer
+    public static void ConnectionHandeler(CancellationToken token)
     {
-        public static void Connectionhandeler()
+        // Setting up the server IP address and port number
+        IPAddress serverAddress = IPAddress.Parse("127.0.0.7");
+        int serverPort = 8000;
+
+        // Creating our server socket
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        // Binding the socket 
+        serverSocket.Bind(new IPEndPoint(serverAddress, serverPort));
+
+        // server listening for incoming connections
+        serverSocket.Listen(5);
+        Console.WriteLine("Server is listening for connections");
+
+        while(true)
         {
-            while (true)
-            {
-                Console.WriteLine("Lokking for connection.");
-                Thread.Sleep(5000); //Wait for 5s
-            }
-        }
-        public static void ThreadProc() {
-            for (int i = 0; i < 10; i++) {
-                Console.WriteLine("ThreadProc: {0}", i);
-                // Yield the rest of the time slice.
-                Thread.Sleep(1000);
-            }
+            // Accepting a client connection
+            Socket clientSocket = serverSocket.Accept();
+            Console.WriteLine("Client connected!");
+
+            // Receiving message from the client
+            byte[] temp = new byte[1024];
+            int clientBytes = clientSocket.Receive(temp);
+            string clientMessage = Encoding.ASCII.GetString(temp, 0, clientBytes);
+            Console.WriteLine("Received data from client: " + clientMessage);
+
+            // Process and perform operations on the received data 
+            // Sending the message back to the client
+            string serverMessage = "Hello, client!";
+            byte[] temp2 = Encoding.ASCII.GetBytes(serverMessage);
+            clientSocket.Send(temp2);
+
+             // Closing the connection
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
         }
 
-        public static void Main()
+        // Close the server socket
+        serverSocket.Close();
+
+        while (!token.IsCancellationRequested)
         {
-            Thread connectionHandeler = new Thread(new ThreadStart(Connectionhandeler));
-            Thread t = new Thread(new ThreadStart(ThreadProc));
-            t.Start();
-            connectionHandeler.Start();
+            Console.WriteLine("Lokking for connection.");
+            Thread.Sleep(2000); //Wait for 2s
         }
+    }
+    public static void Test(CancellationToken token)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(1000);
+        }
+    }
+
+    public static void Main()
+    {
+        CancellationTokenSource tokenSource = new();
+        Thread connectionHandeler = new(() => ConnectionHandeler(tokenSource.Token));
+
+        connectionHandeler.Start();
     }
 }
